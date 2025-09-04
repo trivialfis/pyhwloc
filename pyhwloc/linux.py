a84d5b2882f76afc0c4a8e4181ef0f0bc3962545
Jiaming Yuan <jm.yuan@outlook.com>
2025-09-05 03:39:10 +0800

    Linux-specific.

---
+++ pyhwloc/linux.py
@@ -0,0 +1,82 @@
+# Copyright (c) 2025, NVIDIA CORPORATION.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import ctypes
+
+from .core import (
+    _LIB,
+    _checkc,
+    hwloc_bitmap_t,
+    hwloc_const_cpuset_t,
+    hwloc_cpuset_t,
+    topology_t,
+)
+
+########################
+# Linux-specific helpers
+########################
+
+# https://www.open-mpi.org/projects/hwloc/doc/v2.12.1/a00171.php
+
+_LIB.hwloc_linux_set_tid_cpubind.argtypes = [
+    topology_t,
+    ctypes.c_int,  # pid_t (typically int on Linux)
+    hwloc_const_cpuset_t,
+]
+_LIB.hwloc_linux_set_tid_cpubind.restype = ctypes.c_int
+
+
+def set_tid_cpubind(
+    topology: topology_t, tid: int, cpuset: hwloc_const_cpuset_t
+) -> None:
+    _checkc(_LIB.hwloc_linux_set_tid_cpubind(topology, tid, cpuset))
+
+
+_LIB.hwloc_linux_get_tid_cpubind.argtypes = [
+    topology_t,
+    ctypes.c_int,  # pid_t (typically int on Linux)
+    hwloc_cpuset_t,
+]
+_LIB.hwloc_linux_get_tid_cpubind.restype = ctypes.c_int
+
+
+def get_tid_cpubind(topology: topology_t, tid: int, cpuset: hwloc_cpuset_t) -> None:
+    _checkc(_LIB.hwloc_linux_get_tid_cpubind(topology, tid, cpuset))
+
+
+_LIB.hwloc_linux_get_tid_last_cpu_location.argtypes = [
+    topology_t,
+    ctypes.c_int,  # pid_t (typically int on Linux)
+    hwloc_bitmap_t,
+]
+_LIB.hwloc_linux_get_tid_last_cpu_location.restype = ctypes.c_int
+
+
+def get_tid_last_cpu_location(
+    topology: topology_t, tid: int, cpuset: hwloc_bitmap_t
+) -> None:
+    _checkc(_LIB.hwloc_linux_get_tid_last_cpu_location(topology, tid, cpuset))
+
+
+_LIB.hwloc_linux_read_path_as_cpumask.argtypes = [
+    ctypes.c_char_p,  # const char *path
+    hwloc_bitmap_t,
+]
+_LIB.hwloc_linux_read_path_as_cpumask.restype = ctypes.c_int
+
+
+def read_path_as_cpumask(path: str, cpuset: hwloc_bitmap_t) -> None:
+    path_bytes = path.encode("utf-8")
+    _checkc(_LIB.hwloc_linux_read_path_as_cpumask(path_bytes, cpuset))
---
+++ pyhwloc/tests/test_linux.py
@@ -0,0 +1,70 @@
+# Copyright (c) 2025, NVIDIA CORPORATION.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import platform
+
+import pytest
+
+from pyhwloc.core import (
+    bitmap_alloc,
+    bitmap_free,
+    bitmap_isequal,
+    bitmap_iszero,
+    bitmap_only,
+)
+from pyhwloc.linux import get_tid_cpubind, get_tid_last_cpu_location, set_tid_cpubind
+
+from .test_core import Topology
+
+
+@pytest.mark.skipif(
+    condition=platform.system() != "Linux", reason="Linux-specific test"
+)
+def test_tid_cpubind() -> None:
+    topo = Topology()
+    tid = 0
+
+    # Allocate cpusets for testing
+    original_cpuset = bitmap_alloc()
+    test_cpuset = bitmap_alloc()
+    retrieved_cpuset = bitmap_alloc()
+
+    get_tid_cpubind(topo.hdl, tid, original_cpuset)
+
+    # Create a test CPU set (bind to CPU 0 only)
+    bitmap_only(test_cpuset, 0)
+    # Roundtrip
+    set_tid_cpubind(topo.hdl, tid, test_cpuset)
+    get_tid_cpubind(topo.hdl, tid, retrieved_cpuset)
+
+    assert bitmap_isequal(test_cpuset, retrieved_cpuset)
+
+    # Restore original CPU binding
+    set_tid_cpubind(topo.hdl, tid, original_cpuset)
+
+    bitmap_free(original_cpuset)
+    bitmap_free(test_cpuset)
+    bitmap_free(retrieved_cpuset)
+
+
+@pytest.mark.skipif(
+    condition=platform.system() != "Linux", reason="Linux-specific test"
+)
+def test_get_tid_last_cpu_location() -> None:
+    topo = Topology()
+    cpuset = bitmap_alloc()
+    get_tid_last_cpu_location(topo.hdl, 0, cpuset)
+    assert not bitmap_iszero(cpuset)
+    bitmap_free(cpuset)
