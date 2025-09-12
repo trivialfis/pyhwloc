7c8280e203f44e8b9d7b4663b4250ea01bbff3b9
Jiaming Yuan <jm.yuan@outlook.com>
2025-09-13 02:47:34 +0800

    Test examples.

---
+++ pyhwloc/tests/test_examples.py
@@ -0,0 +1,56 @@
+# Copyright (c) 2025, NVIDIA CORPORATION.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+from __future__ import annotations
+
+import os
+import subprocess
+from functools import cache as fcache
+from pathlib import Path
+
+import pytest
+
+from pyhwloc.hwloc.lib import normpath
+
+tests_dir = Path(normpath(__file__)).parent
+demo_dir = tests_dir.parent.parent / "examples"
+
+
+@fcache
+def has_gpu() -> bool:
+    out = subprocess.run(["nvidia-smi", "-L"], stdout=subprocess.PIPE)
+    gpus = out.stdout.decode("utf-8").strip().splitlines()
+    if not gpus:
+        return False
+    return True
+
+
+def test_intro_low_level() -> None:
+    script = os.path.join(demo_dir, "intro_low_level.py")
+    results = subprocess.check_call(["python", script], stdout=subprocess.PIPE)
+    assert results == 0
+
+
+def test_intro() -> None:
+    script = os.path.join(demo_dir, "intro.py")
+    results = subprocess.check_call(["python", script], stdout=subprocess.PIPE)
+    assert results == 0
+
+
+@pytest.mark.skipif(condition=not has_gpu(), reason="GPU discovery tests.")
+def test_list_gpus() -> None:
+    has_gpu()
+    script = os.path.join(demo_dir, "list_gpus.py")
+    results = subprocess.check_call(["python", script], stdout=subprocess.PIPE)
+    assert results == 0
