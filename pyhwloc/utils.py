96fc5b52a7ad612effc39248b9c8ef4876b6cbef
Jiaming Yuan <jm.yuan@outlook.com>
2025-09-15 03:18:39 +0800

    Memory view.

--- pyhwloc/utils.py
+++ pyhwloc/utils.py
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+from __future__ import annotations
+
+import ctypes
 from collections.abc import Sequence
 from typing import Callable, ParamSpec, TypeVar, Union
 
@@ -38,3 +41,37 @@ def _or_flags(flags: _Flags) -> int:
             r |= f
         flags = r
     return flags
+
+
+ctypes.pythonapi.PyMemoryView_FromMemory.argtypes = (
+    ctypes.c_void_p,
+    ctypes.c_ssize_t,  # size: size of the memory block
+    ctypes.c_int,  # flags: 0x100 for read-only, 0x200 for read/write
+)
+ctypes.pythonapi.PyMemoryView_FromMemory.restype = ctypes.py_object  # Python memoryview
+
+
+def memoryview_from_memory(
+    ptr: ctypes.c_void_p, size: int, read_only: bool
+) -> memoryview:
+    """Create a Python memoryview from a ctypes poitner.
+
+    Parameters
+    ----------
+    ptr :
+        A ctypes Pointer.
+    size :
+        Size in bytes.
+    read_only :
+        Do we need write access to the memory view?
+
+    Returns
+    -------
+    A Python :py:class:`memoryview` object.
+
+    """
+    PyBUF_READ = 0x100
+    PyBUF_WRITE = 0x200
+    flags = PyBUF_READ if read_only else PyBUF_WRITE
+    mv = ctypes.pythonapi.PyMemoryView_FromMemory(ptr, size, flags)
+    return mv
