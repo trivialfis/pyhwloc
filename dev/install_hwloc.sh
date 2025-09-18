f00bde70a091e977135bb61f5e6c21905e04d687
Jiaming Yuan <jm.yuan@outlook.com>
2025-09-19 07:12:06 +0800

    Test a new image.

--- dev/install_hwloc.sh
+++ dev/install_hwloc.sh
@@ -3,5 +3,11 @@
 cd hwloc
 echo "Run autogen"
 ./autogen.sh
-./configure --prefix=$CONDA_PREFIX --disable-nvml
+./configure --prefix=$CONDA_PREFIX --disable-nvml --enable-doxygen
 time make -j$(nproc) install
+
+cd doc
+HWLOC_DOXYGEN_GENERATE_XML=YES doxygen ./doxygen.cfg
+cp -r doxygen-doc/xml /ws/xml
+cd ..				# hwloc
+git clean -xdf
