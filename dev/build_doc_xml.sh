bd900650da74781008ca9629360327800b2738df
Jiaming Yuan <jm.yuan@outlook.com>
2025-09-19 07:59:42 +0800

    rebuild

---
+++ dev/build_doc_xml.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+cd /ws/hwloc/doc
+HWLOC_DOXYGEN_GENERATE_XML=YES doxygen ./doxygen.cfg
+cp -r doxygen-doc/xml /ws/xml
+cd ..				# hwloc
--- dev/install_hwloc.sh
+++ dev/install_hwloc.sh
@@ -5,9 +5,3 @@ echo "Run autogen"
 ./autogen.sh
 ./configure --prefix=$CONDA_PREFIX --disable-nvml --enable-doxygen
 time make -j$(nproc) install
-
-cd doc
-HWLOC_DOXYGEN_GENERATE_XML=YES doxygen ./doxygen.cfg
-cp -r doxygen-doc/xml /ws/xml
-cd ..				# hwloc
-git clean -xdf
