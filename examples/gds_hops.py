4c2c9cb7f018a66d1eb78895ecc39e63ef4589d1
Jiaming Yuan <jm.yuan@outlook.com>
2025-09-26 08:19:10 +0800

    Cleanup GDS hops example.

--- examples/gds_hops.py
+++ examples/gds_hops.py
@@ -12,7 +12,7 @@ Please see the `GDS document
 
 """
 
-from typing import TypeGuard
+from typing import TypeGuard, cast
 
 from pyhwloc import Topology
 from pyhwloc.hwobject import GetTypeDepth, Object, ObjType, OsDevice
@@ -32,6 +32,7 @@ def cnt_hops(gpu: Object, nvme: Object) -> tuple[int, bool]:
     communication.
 
     """
+    print("GPU:", gpu, "<-> NVME:", nvme)
     gpu_ancestors = []
     while gpu.parent is not None:
         gpu_ancestors.append(gpu.parent)
@@ -43,20 +44,42 @@ def cnt_hops(gpu: Object, nvme: Object) -> tuple[int, bool]:
         nvme = nvme.parent
 
     # Find the common ancestor
-    i, j = -1, -1
-    assert gpu_ancestors[i] == nvme_ancestors[j]
+    gpu_ancestors = list(reversed(gpu_ancestors))
+    nvme_ancestors = list(reversed(nvme_ancestors))
+    i, j = 0, 0
+    assert gpu_ancestors[i] == nvme_ancestors[j]  # must have the same root
     while (
-        -i <= len(gpu_ancestors)
-        and -j <= len(nvme_ancestors)
+        i < len(gpu_ancestors)
+        and j < len(nvme_ancestors)
         and gpu_ancestors[i] == nvme_ancestors[j]
     ):
-        i -= 1
-        j -= 1
-    assert gpu_ancestors[i + 1] == nvme_ancestors[j + 1]
+        i += 1
+        j += 1
+    assert i >= 1 and j >= 1, (i, j)
     # It might be better to check whether this is a PCI bridge instead of checking
     # whether this is a host device.
-    crossed = gpu_ancestors[i + 1].is_package() or gpu_ancestors[i + 1].is_machine()
-    return (-(i + 1) + -(j + 1)), crossed
+    crossed = gpu_ancestors[i - 1].is_package() or gpu_ancestors[i - 1].is_machine()
+    # Assuming GPU is an OS device attached to a PCI device. There are GPUs that are
+    # connected through nvlink-C2C, which does not use PCI.
+    assert gpu_ancestors[-1].is_pci_device() and nvme_ancestors[-1].is_pci_device()
+    gpu_path = []
+    for k in range(i - 1, len(gpu_ancestors)):
+        gpu_path.append(str(gpu_ancestors[k]))
+    print("- GPU path:", " -> ".join(list(reversed(gpu_path))))
+    nvme_path = []
+
+    for k in range(j - 1, len(nvme_ancestors)):
+        nvme_path.append(str(nvme_ancestors[k]))
+    print("- NVME path:", " -> ".join(list(reversed(nvme_path))))
+    print()
+
+    # +1 for the common ancestor. -1 for removing the PCI device. OS device is a
+    # software device, which is "attached" to the PCI device. But they are the same
+    # thing.
+    n_hops = (len(gpu_ancestors) - i - 1) + (len(nvme_ancestors) - j - 1) + 1
+    # if n_hops == 0:
+    #     n_hops = len(gpu_ancestors) + len(nvme_ancestors) - 1
+    return n_hops, crossed
 
 
 def pprint(table: list[list]) -> None:
@@ -70,7 +93,7 @@ def pprint(table: list[list]) -> None:
                 print(f"{row[j]}", end="")
             else:
                 print(f"{row[j]:7},", end="")
-        print("\n")
+        print()
 
 
 if __name__ == "__main__":
@@ -96,8 +119,8 @@ if __name__ == "__main__":
 
         hops_rows: list[list[str | int]] = []
         cross_rows: list[list[str | bool]] = []
-        hops_rows.append([dev.name for dev in nvmes])  # type: ignore
-        cross_rows.append([dev.name for dev in nvmes])  # type: ignore
+        hops_rows.append([cast(str, dev.name) for dev in nvmes])
+        cross_rows.append([cast(str, dev.name) for dev in nvmes])
         for i, gpu in enumerate(gpus):
             assert gpu.name is not None
             hops_row: list[str | int] = [gpu.name]
